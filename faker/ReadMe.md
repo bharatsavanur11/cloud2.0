922c8120683ee56f775fd6a4ef69d3d8e8a97f9e
bharatsavanur11 <bharat.savanur@gmail.com>
2023-04-22 17:55:56 +0530

    Updated ReadMe.md and Initial Checkin for full text search

---
+++ faker/ReadMe.md
@@ -0,0 +1,5 @@
+This project containsL
+* Connecttion to MongoDb Atlas
+* Using MongoDB Text Search 
+* Generate Fake Data
+* Querying the saved fake data using mongodb search API like elasticsearch.
--- faker/src/main/java/com/example/business/MongoService.java
+++ faker/src/main/java/com/example/business/MongoService.java
@@ -1,6 +1,7 @@
 package com.example.business;
 
 import com.example.MongoConfig;
+import com.hazelcast.com.google.common.collect.Lists;
 import com.hazelcast.core.HazelcastInstance;
 import com.hazelcast.map.IMap;
 import io.micrometer.core.instrument.LongTaskTimer;
@@ -17,46 +18,50 @@ import java.util.logging.Logger;
 
 
 @Component
-public class MongoService{
+public class MongoService {
 
 
-        private static final Logger log = Logger.getLogger("QuoteController");
+    private static final Logger log = Logger.getLogger("QuoteController");
 
-        private static long DELAY_BY_TIME = 1000;
+    private static long DELAY_BY_TIME = 1000;
 
-        @Qualifier("testDb")
-        @Autowired
-        private  MongoTemplate mongoTemplate;
+    @Qualifier("testDb")
+    @Autowired
+    private MongoTemplate mongoTemplate;
 
-        @Qualifier("airBnbDb")
-        @Autowired
-        private  MongoTemplate mongoTemplateAirBnb;
+    @Qualifier("airBnbDb")
+    @Autowired
+    private MongoTemplate mongoTemplateAirBnb;
 
 
+    public List<Quote> findAllQuotes() {
+        return mongoTemplate.findAll(Quote.class, "quotes");
+    }
 
-        public List<Quote> findAllQuotes() {
-              return mongoTemplate.findAll(Quote.class,"quotes");
-        }
+    public List<String> getAirBnbData() {
+        System.out.print("Fetching Air BnB Data");
+        return mongoTemplateAirBnb.findAll(String.class, "listingsAndReviews");
+    }
 
-        public List<String> getAirBnbData() {
-                System.out.print("Fetching Air BnB Data");
-                return mongoTemplateAirBnb.findAll(String.class,"listingsAndReviews");
-        }
+    public List<String> fetchMatchingSearchByFilterCriteria(String field, String searchText, String collectionName) {
+        Query searchFreeTextQuery = new Query();
+        searchFreeTextQuery.addCriteria(Criteria.where(field).regex(searchText));
+        SimpleMeterRegistry registry = new SimpleMeterRegistry();
+        LongTaskTimer longTaskTimer = LongTaskTimer.builder("3rdPartyService").register(registry);
+        LongTaskTimer.Sample currentTaskId = longTaskTimer.start();
+        List<String> returnData = mongoTemplateAirBnb.find(searchFreeTextQuery, String.class, collectionName);
+        long timeElapsed = currentTaskId.stop();
+        System.out.println("Time Elapsed in milliseconds" + timeElapsed / ((int) 1e6));
+        //System.out.print("Data Returned"+returnData);
+        return returnData;
+    }
 
-        public List<String> fetchMatchingSearchByFilterCriteria(String field, String searchText,String collectionName) {
-                Query searchFreeTextQuery = new Query();
-                searchFreeTextQuery.addCriteria(Criteria.where(field).regex(searchText));
-                SimpleMeterRegistry registry = new SimpleMeterRegistry();
-                LongTaskTimer longTaskTimer = LongTaskTimer.builder("3rdPartyService").register(registry);
-                LongTaskTimer.Sample currentTaskId = longTaskTimer.start();
-                List<String>  returnData =  mongoTemplateAirBnb.find(searchFreeTextQuery,String.class,collectionName);
-                long timeElapsed = currentTaskId.stop();
-                System.out.println("Time Elapsed in milliseconds" + timeElapsed/((int) 1e6));
-                //System.out.print("Data Returned"+returnData);
-                return returnData;
-        }
+    public void saveQuotes(List<Quote> quotes) {
+        mongoTemplate.insert(quotes, "quotes");
+    }
 
-        public void saveQuotes(List<Quote> quotes) {
-                mongoTemplate.insert(quotes,"quotes");
-        }
+    public List<String> fetchSearchTextData(String text) {
+
+        return Lists.newArrayList();
+    }
 }
