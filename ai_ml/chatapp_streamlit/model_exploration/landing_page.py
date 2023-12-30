10f8062fd869ac1065cac6e5744a4970ba06a279
bharatsavanur11 <bharat.savanur@gmail.com>
2023-12-30 08:46:26 +0530

    Updating the first Summarization Task and wrapped up GIT integration with Trello

--- ai_ml/chatapp_streamlit/model_exploration/landing_page.py
+++ ai_ml/chatapp_streamlit/model_exploration/landing_page.py
@@ -6,11 +6,14 @@ def main():
     st.sidebar.title("Chatbot Menu")
 
     # Add options to the sidebar menu
-    task_type = st.sidebar.selectbox("Select Option", ["Please Select Task to Perform","Summarization Task", "Generate Content","Sentiment Analysis", "Chatbot Text",])
+    task_type = st.sidebar.selectbox("Task Type", ["Please Select Task to Perform","Summarization Task", "Generate Content","Sentiment Analysis", "Chatbot Text",])
 
     st.title("Welcome to the Model Exploration App for different LLM Tasks")
     st.write("This is the place for AI/ML enthusiasts to play around.")
 
+    if task_type == "Summarization Task":
+        st.write("Summarization Task")
+        applicable_models = st.sidebar.selectbox("Please select Model Type",["GPT 2","GPT 3","FLAN T5"])
 
 if __name__ == "__main__":
     main()
